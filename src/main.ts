cec2415220f605a1915306184a99f2be1f17977d
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2024-09-25 20:46:59 -0400

    Setting it up

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import './assets/main.css'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(ElementPlus)
 app.use(router)
-
 app.mount('#app')
---
+++ src/views/login_sign/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ElButton } from 'element-plus'
+</script>
+
+<template>
+  <header>
+
+  </header>
+  <main>
+    <h1>This is homepage</h1>
+    <el-button type="primary">Primary</el-button>
+  </main>
+</template>
